3c7a557b371e8d70640784411d8a2460ca5d470e
Reiyan <reiyan@gmx.de>
2019-08-15 17:04:36 +0200

    forgot versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='pflacco',  
-     version='0.2',
+     version='0.3',
      author="Raphael Patrick Prager",
      author_email="raphael.prager@gmx.de",
      description="An python interface to the R package flacco for computing ELA features.",
