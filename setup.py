4dc4ae5d0812a5c4a6cc06282555ffc40144a775
Reiyan <reiyan@gmx.de>
2022-08-11 18:13:06 +0200

    added dependencies to requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
      name='pflacco',  
-     version='0.3',
+     version='1.0.0',
      author="Raphael Patrick Prager",
      author_email="raphael.prager@gmx.de",
      description="An python interface to the R package flacco for computing ELA features.",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     license = 'MIT',
-     install_requires=[
-         'rpy2>=3.0.5',
-         'numpy>=1.17.0'
-     ],
+     license='MIT',
+     install_requires=required,
      url="https://github.com/javatechy/dokr",
      packages=setuptools.find_packages(),
      classifiers=[
