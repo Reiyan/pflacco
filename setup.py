950d7fdbe1f86f545cb8ca1f4a3b1995e376e2a0
Reiyan <reiyan@gmx.de>
2022-08-24 13:56:05 +0200

    8: rpy2 dependency fix

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
      name='pflacco',  
-     version='0.3',
+     version='0.4.1',
      author="Raphael Patrick Prager",
      author_email="raphael.prager@gmx.de",
      description="An python interface to the R package flacco for computing ELA features.",
      long_description=long_description,
      long_description_content_type="text/markdown",
      license = 'MIT',
-     install_requires=[
-         'rpy2>=3.0.5',
-         'numpy>=1.17.0'
-     ],
-     url="https://github.com/javatechy/dokr",
+     install_requires=required,
+     url="https://pypi.org/project/pflacco/",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
