1807de566335acd773cc54bc19384378e8e16650
r_prag01 <raphael.prager@wi.uni-muenster.de>
2023-03-15 00:51:47 +0100

    #17 fixed import of pickle file

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,5 @@ RSC = os.path.join('tests', 'resources')
 
 @pytest.fixture(scope="package")
 def x_samples():
-    X = pd.read_csv(os.path.join(RSC, 'init_sample.pkl'))
+    X = pd.read_pickle(os.path.join(RSC, 'init_sample.pkl'))
     return X
