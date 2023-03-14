3f8a76038116c8fc1b1ad730292f3ca8f8c55d9a
r_prag01 <raphael.prager@wi.uni-muenster.de>
2023-03-15 00:46:20 +0100

    #17 added missing unit test files

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,3 @@ RSC = os.path.join('tests', 'resources')
 def x_samples():
     X = pd.read_csv(os.path.join(RSC, 'init_sample.csv'))
     return X
-
-#@pytest.fixture
-#def ela_classical_feature_values():
-#    X = pd.read_csv(os.path.join(RSC, 'test_classical_ela_features.csv'))
-#    return X
-
